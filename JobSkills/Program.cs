20e84b5409dde43bdda05d951c09625f2358b92c
Littu Theresa S <littutheresas@outlook.com>
2020-08-18 14:43:22 +0100

    Initial Commit

---
+++ JobSkills/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks.Sources;
+
+namespace JobSkills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> job = new List<string>() 
+            {
+                "flexible working hours",
+                "office based two days per week",
+                "react developer"
+            };
+
+            List<string> individual = new List<string>()
+            {
+               "backend developer",
+                "cloud experience",
+                "AWS certified"
+            };
+
+            SelectionPanel selectionPanel = new SelectionPanel();
+            int score = selectionPanel.IdealMatch(job, individual);
+            
+            if(score == 100)
+            {
+                Console.WriteLine("Ideal match with Selection Score: " + score);
+            }
+
+            else if(score == 0)
+            {
+                Console.WriteLine("Not match with Selection Score: " + score);
+            }
+
+            else
+            {
+                Console.WriteLine("Selection Score: " + score);
+            }
+
+            Console.ReadLine();
+
+        }
+
+       
+    }
+}
---
+++ JobSkills/SelectionPanel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobSkills
+{
+    class SelectionPanel
+    {
+        #region Fields
+        List<string> requiredJobSkills = new List<string>()
+        {
+            "remote",
+            "flexible working hours",
+            "office based two days per week",
+            "react developer"
+        };
+
+        List<string> requiredPersonSkills = new List<string>()
+        {
+            "wants to work remote",
+            "react experience",
+            "looking for frontend",
+            "backend developer",
+            "cloud experience",
+            "AWS certified"
+        };
+        #endregion
+
+        #region Methods
+
+        // function to check for ideal match
+        public int IdealMatch(List<string> job, List<string> individual)
+        {
+            int Score = 0;
+            //check if the individual's job skill is in preferred job skill; if so, add 10 points to score for each skill
+            foreach (string skills in job)
+            {
+                if (requiredJobSkills.Contains(skills))
+                {
+                    Score += 10;
+                }
+            }
+
+            //check if the individual's charcteristics is in preferred person skill; if so, add 10 points to score for each skill
+            foreach (string skills in individual)
+            {
+                if (requiredPersonSkills.Contains(skills))
+                {
+                    Score += 10;
+                }
+            }
+
+            return Score;
+        }
+        #endregion
+    }
+}
